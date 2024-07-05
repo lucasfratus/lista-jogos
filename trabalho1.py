8d808d6055ba04ade1eb0b3b1add15b6c54fb56f
lucasfratus <134955580+lucasfratus@users.noreply.github.com>
2024-07-05 19:51:36 -0300

    Update trabalho1.py

--- trabalho1.py
+++ trabalho1.py
@@ -9,24 +9,26 @@ def leia_reg(entrada):
     if entrada.tell() < SIZEOF_HEADER:
         entrada.seek(4)             # Coloca o ponteiro no primeiro registro, pulando o header.
     offset = entrada.read(2)        # Leitura do byte offset
-    offset = int.from_bytes(offset)
+    posicao_ponteiro = entrada.tell()   # Guarda a posição do ponteiro após a leitura do byteoffset
+    offset = int.from_bytes(offset) # Transforma o offset(que está em bytes) em int.
     if offset > 0:
         buffer = entrada.read(offset)
         buffer = buffer.decode()
-        return buffer
+        print(posicao_ponteiro)
+        return buffer, posicao_ponteiro
     else:
-        return ''
+        return '', posicao_ponteiro
     
     
 def busca_chave(entrada, chave: str):
     achou = False
-    registro = leia_reg(entrada)
+    registro = leia_reg(entrada)[0]
     while registro != '' and not achou:
         identificador = registro.split(sep='|')[0]
         if identificador == chave:
             achou = True
         else:
-            registro = leia_reg(entrada)
+            registro = leia_reg(entrada)[0]
     if achou == True:
         registro = registro.split(sep='|')
         buffer = ''
@@ -38,4 +40,4 @@ def busca_chave(entrada, chave: str):
     else:
         print('Erro! Identificador não encontrado.')
 
-busca_chave(entrada,'9')
+busca_chave(entrada,'103')
